dd7644a99b4d732e21c5b0cfa719387bcd0916ff
fedecurto98 <fedecurto1@gmail.com>
2020-08-11 17:43:48 -0300

    se agrego comentario

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
+        //hola
     }
 
 
