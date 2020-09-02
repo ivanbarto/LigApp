63306a7fadace93c6fcdb212645158750807e844
fedecurto98 <fedecurto1@gmail.com>
2020-09-02 17:20:17 -0300

    Tabla no da error pero no carga valores

--- src/main/java/sample/MainMenuController.java
+++ src/main/java/sample/MainMenuController.java
@@ -3,15 +3,20 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainMenuController {
+public class MainMenuController{
     @FXML
     private Button btnCancel;
     @FXML
@@ -22,6 +27,30 @@ public class MainMenuController {
     private TableView<Player> tbvPlayer;
     @FXML
     private TableView<Team> tbvTeam;
+    @FXML
+    private TableColumn<Player, Integer> idPlayerColumn;
+    @FXML
+    private TableColumn<Player, String> firstNameColumn ;
+    @FXML
+    private TableColumn<Player, String> lastNameColumn;
+    @FXML
+    private TableColumn<Player, String> dniColumn;
+    @FXML
+    private TableColumn<Player, String> ageColumn;
+    @FXML
+    private TableColumn<Player, Integer> idTeamColumn;
+
+
+
+    PlayerQueries playerQueries;
+
+    public MainMenuController() {
+        playerQueries = new PlayerQueries();
+        fillTable();
+
+
+
+    }
 
     public void btnCancelOnAction(ActionEvent event){
         Stage stage = (Stage) btnCancel.getScene().getWindow();
@@ -40,6 +69,8 @@ public class MainMenuController {
 
         window.setScene(createPlayerScene);
         window.show();
+
+
     }
 
     public void btnCreateTeamOnAction(ActionEvent event) throws IOException {
@@ -53,5 +84,27 @@ public class MainMenuController {
         window.show();
     }
 
+    public void fillTable(){
+        tbvPlayer = new TableView<>();
+        idPlayerColumn = new TableColumn<>();
+        firstNameColumn = new TableColumn<>();
+        lastNameColumn = new TableColumn<>();
+        dniColumn = new TableColumn<>();
+        ageColumn = new TableColumn<>();
+        idTeamColumn = new TableColumn<>();
+        idPlayerColumn.setCellValueFactory(new PropertyValueFactory<>("idPlayer"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        dniColumn.setCellValueFactory(new PropertyValueFactory<>("DNI"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
+        idTeamColumn.setCellValueFactory(new PropertyValueFactory<>("idTeam"));
+        tbvPlayer.setItems(playerQueries.getPlayers());
+        tbvPlayer.getColumns().add(idPlayerColumn);
+        tbvPlayer.getColumns().add(firstNameColumn);
+        tbvPlayer.getColumns().add(lastNameColumn);
+        tbvPlayer.getColumns().add(dniColumn);
+        tbvPlayer.getColumns().add(ageColumn);
+        tbvPlayer.getColumns().add(idTeamColumn);
+    }
 }
 
